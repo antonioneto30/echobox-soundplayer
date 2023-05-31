5b26e5e3baf4506fa9669694c427d97afe10950a
antonioneto30 <antonioneto3000@gmail.com>
2023-05-30 23:23:08 -0300

    wip

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function playSoundPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function playSoundClap() {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+
+
 document.querySelector('.tecla_pom');
+document.querySelector('.tecla_clap');
+
