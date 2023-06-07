d84185c4947d5c9ead2b4dfc74b967a541bfdfea
antonioneto30 <antonioneto3000@gmail.com>
2023-06-07 20:28:39 -0300

    wip

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+function playSound (idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+    contador = contador + 1;
+
+}
+
 /* EXEMPLO DE CÓDIGO POM E CLAP
 
 function playSoundPom () {
@@ -14,19 +36,3 @@ document.querySelector('.tecla_pom').onclick = playSoundPom;
 document.querySelector('.tecla_clap').onclick = playSoundClap;
 
 */
-
-function playSoundPom () {
-    document.querySelector('#som_tecla_pom').play();
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = playSoundPom;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
