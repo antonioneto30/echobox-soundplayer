c6aa43ef5ba657b2448d8be4d529ecfd71de88ac
antonioneto30 <antonioneto3000@gmail.com>
2023-06-07 20:34:38 -0300

    wip

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-function playSound (idElementAudio) {
-    document.querySelector(idElementAudio).play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-  const tecla = listaDeTeclas[contador];
-  const instrumento = tecla.classList[1];
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-  const idAudio = `#som_${instrumento}`;
-
-  tecla.onclick = function () {
-    playSound(idAudio);
-  }
-    contador = contador + 1;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
 }
 
